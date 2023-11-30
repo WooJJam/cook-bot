883ff1e9b4f80c5f2c0730fcd858c3fd2c15d37e
WooJJam <jaemin5548@naver.com>
2023-12-01 01:57:56 +0900

    [Feat] 채팅을 위한 Message 클래스 구현

--- app/src/main/java/com/example/cook_bot/Message.java
+++ app/src/main/java/com/example/cook_bot/Message.java
@@ -1,19 +1,37 @@
 package com.example.cook_bot;
 
 public class Message {
+    public static String SENT_BY_ME = "me";
+    public static String SENT_BY_BOT = "bot";
     private String text;
-    private boolean isUser;
+//    private boolean isUser;
+    private String sendBy;
 
-    public Message(String text, boolean isUser) {
+    public Message(
+            String text,
+//            boolean isUser,
+            String sendBy
+    ) {
         this.text = text;
-        this.isUser = isUser;
+//        this.isUser = isUser;
+        this.sendBy = sendBy;
     }
 
-    public String getText() {
+    public String getMessage() {
         return text;
     }
 
-    public boolean isUser() {
-        return isUser;
+    public void setMessage(String text) {
+        this.text = text;
+    }
+
+//    public boolean isUser() {
+//        return isUser;
+//    }
+    public String getSendBy() {
+        return sendBy;
+    }
+    public void setSendBy(String sendBy) {
+        this.sendBy = sendBy;
     }
 }
